ff1385e4b00053ca55a2124e89ee776fd7e12dc4
silvercloud1442 <47461834+silvercloud1442@users.noreply.github.com>
2023-05-19 13:53:57 +0300

    Backup + transfer

--- Backup-DB.ps1
+++ Backup-DB.ps1
@@ -1,18 +1,17 @@
 ﻿#Читаем данные из конфига
-$ini = C:\project\Get-iniFile.ps1 C:\project\config.ini
+﻿$ini = C:\project\Get-iniFile.ps1 C:\project\config.ini
 
 #пихаем конфиг в переменные
 $sqlServerInstance = $ini["Server1"]["sqlServerInstance"]
-$sourceDatabase = $ini["Server1"]["sourceDatabase"] 
-$sourceServer = $ini["Server1"]["sourceServer"]
+$sourceDatabase = $ini["Server1"]["sourceDatabase"]
 $backupPath = $ini["Server1"]["backupPath"]
 
 $targetServer = $ini["Server2"]["targetServer"]
 $targetPath = $ini["Server2"]["targetPath"]
 
 #если папок нет, то создаем
-if (!(Test-Path $sourceServer\$backupPath)) {
-    New-Item -Path $sourceServer\$backupPath -ItemType Directory
+if (!(Test-Path $backupPath)) {
+    New-Item -Path $backupPath -ItemType Directory
 }
 
 if (!(Test-Path $targetServer\$targetPath)) {
@@ -20,17 +19,10 @@ if (!(Test-Path $targetServer\$targetPath)) {
 }
 
 # бэкап
-Backup-SqlDatabase -ServerInstance $sqlServerInstance -Database $sourceDatabase -BackupFile $sourceServer\$backupPath\backup.bak
+Backup-SqlDatabase -ServerInstance $sqlServerInstance -Database $sourceDatabase -BackupFile $backupPath\backup.bak -Initialize
 
 # копия на другой сервер
-Copy-Item -Path $sourceServer\$backupPath\backup.bak -Destination $targetServer\$targetPath\backup.bak
-
-
-
 #ПРОБЛЕМЫ 
-
-#1) Если бэкап и копирование в 1 потоке - нихера не работает
-#Решения: два потока? починить в 1 потоке?
-
 #2) Везде используются абсолютные пути
-#Решения: смириться?
+cd $backupPath
+Copy-Item -Path $backupPath\backup.bak -Destination $targetServer\$targetPath\backup.bak
