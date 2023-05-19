207eef7081d2b7a5c0d85e40223c4db2f78954de
silvercloud1442 <47461834+silvercloud1442@users.noreply.github.com>
2023-05-19 13:55:03 +0300

    Update Backup-DB.ps1

--- Backup-DB.ps1
+++ Backup-DB.ps1
@@ -1,7 +1,5 @@
-﻿#Читаем данные из конфига
 ﻿$ini = C:\project\Get-iniFile.ps1 C:\project\config.ini
 
-#пихаем конфиг в переменные
 $sqlServerInstance = $ini["Server1"]["sqlServerInstance"]
 $sourceDatabase = $ini["Server1"]["sourceDatabase"]
 $backupPath = $ini["Server1"]["backupPath"]
@@ -9,7 +7,6 @@ $backupPath = $ini["Server1"]["backupPath"]
 $targetServer = $ini["Server2"]["targetServer"]
 $targetPath = $ini["Server2"]["targetPath"]
 
-#если папок нет, то создаем
 if (!(Test-Path $backupPath)) {
     New-Item -Path $backupPath -ItemType Directory
 }
@@ -18,11 +15,7 @@ if (!(Test-Path $targetServer\$targetPath)) {
     New-Item -Path $targetServer\$targetPath -ItemType Directory
 }
 
-# бэкап
 Backup-SqlDatabase -ServerInstance $sqlServerInstance -Database $sourceDatabase -BackupFile $backupPath\backup.bak -Initialize
 
-# копия на другой сервер
-#ПРОБЛЕМЫ 
-#2) Везде используются абсолютные пути
 cd $backupPath
 Copy-Item -Path $backupPath\backup.bak -Destination $targetServer\$targetPath\backup.bak
